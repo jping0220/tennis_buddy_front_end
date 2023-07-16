799a5cd4690c0f9f1d8cdbbbe9e7daac1547bd6f
Jiaping Chen <jiapingchen17@gmail.com>
2023-07-16 16:41:24 -0700

    added one more hard code user to display in the page successfully

--- src/App.js
+++ src/App.js
@@ -2,18 +2,28 @@ import React from "react";
 import TennisUser from "./components/TennisUser";
 
 function App() {
-  const user = {
+  const user = [{
     id: 1,
     name: 'Ping',
     email: 'jping@gmail.com',
-    tennis_level: '3.5',
+    tennis_level: 3.5,
     zip_code: '12345',
     preference: 'Singles and night time',
-  };
+  },
+  {
+    id: 2,
+    name: 'Laura',
+    email: 'Laura@gmail.com',
+    tennis_level: 3.0,
+    zip_code: '54321',
+    preference: 'Weekends in the morning',
+    }];
+  
+
     return (
       <div>
         <h1>Tennis User Management</h1>
-        <TennisUser user={user} />
+        {user.map((user) => (<TennisUser key={user.id} user={user} />))}
       </div>
     );
   };
--- src/components/TennisUser.js
+++ src/components/TennisUser.js
@@ -5,10 +5,10 @@ const TennisUser = ({ user }) => {
         <div>
             <h2>{user.name}</h2>
             <p>ID: {user.id}</p>
-            <p>Email:{user.email}</p>
-            <p>Tennis Level {user.tennis_level}</p>
+            <p>Email: {user.email}</p>
+            <p>Tennis Level: {user.tennis_level}</p>
             <p>Zip Code: {user.zip_code}</p>
-            <p>Preference {user.preference}</p>
+            <p>Preference: {user.preference}</p>
         
         </div>
     );
