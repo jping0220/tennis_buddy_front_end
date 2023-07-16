99d12ff208b31ba131b0f223679c2178293a04bc
Jiaping Chen <jiapingchen17@gmail.com>
2023-07-16 15:59:19 -0700

    created NewUserForm,TennisUser, UserLis components and connect tennisuser to app and succcfully showed on  page

---
+++ src/TennisUser.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const TennisUser = ({ user }) => {
+    return (
+        <div>
+
+            <h2>{user.name}</h2>
+            <p>ID: {user.id}</p>
+            <p>Email:{user.email}</p>
+            <p>Tennis Level {user.tennis_level}</p>
+            <p>Zip Code: {user.zip_code}</p>
+            <p>Preference {user.preference}</p>
+        
+        </div>
+    );
+};
+
+export default TennisUser;
