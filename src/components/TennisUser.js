93f37748bb15491124df9410b0e7a75adc9324f8
Jiaping Chen <jiapingchen17@gmail.com>
2023-07-29 12:20:18 -0700

    edit the search button

--- src/components/TennisUser.js
+++ src/components/TennisUser.js
@@ -5,7 +5,7 @@ const TennisUser = ({ user }) => {
   return (
     <Card className='tennis-user-card' style={{ width: "30rem" }}>
       <Card.Body>
-        <Card.Title>name:{user.name}</Card.Title>
+        <Card.Title className="user-info-name">Name:{user.name}</Card.Title>
         <Card.Subtitle className="user-info-zip-code">Zip Code: {user.zip_code}</Card.Subtitle>
         <Card.Subtitle className="user-info-email">Email: {user.email}</Card.Subtitle>
         <Card.Subtitle className="user-info-skill-level">Skill Level: {user.tennis_level}</Card.Subtitle>
