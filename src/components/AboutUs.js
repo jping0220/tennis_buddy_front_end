bd5adb4e340b2680e963b55737a480c15ac82839
Jiaping Chen <jiapingchen17@gmail.com>
2023-07-30 12:29:12 -0700

    place the about us card in the middle of page

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -4,19 +4,20 @@ import Card from "react-bootstrap/Card";
 
 export const AboutUs = () => {
   return (
-    <Card style={{ width: "18rem" }}>
-      <Card.Img variant="top" src={Ping_Laura} />
-      <Card.Body>
-        <Card.Title>Ping and Laura</Card.Title>
-        <Card.Text>
-          We have problem , we wanted play tennis! and we decided to solve
-          together.
-        </Card.Text>
-        
-        
-      </Card.Body>
-    </Card>
-    // </div>
+    <div className="container">
+      <Card style={{ width: "18rem" }}>
+        <Card.Img variant="top" src={Ping_Laura} />
+        <Card.Body>
+          <Card.Title>Ping and Laura</Card.Title>
+          <Card.Text>
+            We have problem , we wanted play tennis! and we decided to solve
+            together.
+          </Card.Text>
+          
+          
+        </Card.Body>
+      </Card>
+    </div>
   );
 };
 
