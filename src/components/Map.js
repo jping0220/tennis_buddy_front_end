3c72f6c321180930657b5f9afceb3f2c19e26827
Jiaping Chen <jiapingchen17@gmail.com>
2023-08-03 15:55:58 -0700

    able to show the name and tennis_level on the map when marker is being clicked

--- src/components/Map.js
+++ src/components/Map.js
@@ -24,38 +24,19 @@ export const MapDisplay = ({ searchResult }) => {
 
   const [mapRef, setMapRef] = useState();
   const [isOpen, setIsOpen] = useState(false);
-  const [infoWindowData, setInfoWindowData] = useState();
+  const [infoWindowData, setInfoWindowData] = useState({});
+  const [clickedMarkerIndex, setClickedMarkerIndex] = useState(null);
 
+  // const markers = [
+  //   { address: "data info from backend"},
+  // ];
 
-  const getCoordinates = async (address) => {
-    const apiKey = process.env.REACT_APP_GOOGLE_API_KEY_2;
-    try {
-      const response = await fetch(
-        `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${apiKey}`
-      );
-  
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-  
-      const data = await response.json();
-      const latitude = data.results[0]?.geometry?.location?.lat || null;
-      const longitude = data.results[0]?.geometry?.location?.lng || null;
-      
-      return {latitude,longitude};
-    
-    } catch (error) {
-      console.error("Error fetching coordinates:", error);
-      return { latitude: null, longitude: null };
-    }
-  };
-  
-  // use a function to iterate over the searchResdult and for each zip code, call the APi to get lat and long.
+// use a function to iterate over the searchResdult and for each zip code, call the APi to get lat and long.
   // Once the coordinates are fetched,
   //they are stored in the latLngList state, which can be used to render the map and markers.
   const fetchCoordinates = async () => {
     const promises = searchResult.map((result) =>
-      getCoordinates(result.zip_code)
+      getCoordinates(result.zip_code,result.name,result.tennis_level)
     );
     
     const coordinates = await Promise.all(promises);
@@ -73,25 +54,55 @@ export const MapDisplay = ({ searchResult }) => {
 
 
 
-  // handle markers:
-  const onMapLoad = (map) => {
-    setMapRef(map);
-    const bounds = new google.maps.LatLngBounds();
-    markers?.forEach(({ lat, lng }) => bounds.extend({ lat, lng }));
-    map.fitBounds(bounds);
+  const getCoordinates = async (zipCode,name,tennis_level) => {
+    const apiKey = process.env.REACT_APP_GOOGLE_API_KEY_2;
+    try {
+      const response = await fetch(
+        `https://maps.googleapis.com/maps/api/geocode/json?address=${zipCode}&key=${apiKey}`
+      );
+  
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+  
+      const data = await response.json();
+      const latitude = data.results[0]?.geometry?.location?.lat || null;
+      const longitude = data.results[0]?.geometry?.location?.lng || null;
+      
+      return {latitude,longitude, name, tennis_level};
+    
+    } catch (error) {
+      console.error("Error fetching coordinates:", error);
+      return { latitude: null, longitude: null };
+    }
   };
+  
+  
 
-  const handleMarkerClick = (id, lat, lng, address) => {
+
+  // handle markers:
+  // const onMapLoad = (map) => {
+  //   setMapRef(map);
+  //   const bounds = new google.maps.LatLngBounds();
+  //   markers?.forEach(({ lat, lng }) => bounds.extend({ lat, lng }));
+  //   map.fitBounds(bounds);
+  // };
+
+
+
+  const handleMarkerClick = (id, lat, lng, name, tennis_level) => {
     mapRef?.panTo({ lat, lng });
-    setInfoWindowData({ id, address });
+    setInfoWindowData({ id, name, tennis_level });
+    setClickedMarkerIndex(id);
     setIsOpen(true);
   };
 
-
+ 
 
 
   return (
     <div className="App">
+      {console.log('isOpen:', isOpen, 'clickedMarkerIndex:', clickedMarkerIndex)}
       {!isLoaded ? (
         <h1>Loading...</h1>
       ) : (
@@ -99,28 +110,33 @@ export const MapDisplay = ({ searchResult }) => {
           mapContainerClassName="map-container"
           center={center}
           zoom={10}
-          onLoad={onMapLoad}
+          // onLoad={onMapLoad}
           onClick={() => setIsOpen(false)}
         >
-          {latLngList.map((position, index) => (
-            <Marker
-              key={index}
-              position={{ lat: position.latitude, lng: position.longitude }}
-              onClick={() => {
-                handleMarkerClick(ind, lat, lng, address);
+            {latLngList.map((position, index) => (
+              <Marker
+                key={index}
+                position={{ lat: position.latitude, lng: position.longitude }}
+                onClick={() => {
+                  handleMarkerClick(index, position.latitude, position.longitude, position.name, position.tennis_level);
+                }}
+              />
+            ))}
+            {isOpen && infoWindowData?.id !== null && (
+              <InfoWindow
+              position={{
+                lat: latLngList[clickedMarkerIndex]?.latitude,
+                lng: latLngList[clickedMarkerIndex]?.longitude,
               }}
-            >
-          ))}
-          {isOpen && infoWindowData?.id === ind && (
-            <InfoWindow
               onCloseClick={() => {
                 setIsOpen(false);
               }}
-            >
-              <h3>{infoWindowData.address}</h3>
-            </InfoWindow>
-              )}
-              </Marker>
+              >
+              <div>
+                  <h3>{infoWindowData.name},{infoWindowData.tennis_level}</h3>
+              </div>
+              </InfoWindow>
+              )}   
           {/* <Marker position={{ lat: 47.608013, lng: -122.335167 }}></Marker> */}
         </GoogleMap>
       )}
